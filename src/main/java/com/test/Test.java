9817b262a7ca4e9f8fdb853abd5bbb519d1b4c03
yipanGit <2074955944@qq.com>
2018-05-24 17:30:02 +0800

    Initial commit

---
+++ src/main/java/com/test/Test.java
@@ -0,0 +1,24 @@
+package com.test;
+
+public class Test {
+    private String name;
+
+    public Test(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Test{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
